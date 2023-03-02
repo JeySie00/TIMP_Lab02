3893bca96bcb6df2bb49c88a40a58c8174026c82
JeySie00 <86301125+JeySie00@users.noreply.github.com>
2023-03-02 09:54:47 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main(int argc, char** argv){
-    std::string name; //User name
-    std::cin >> name; //Input user name
-    std::cout << "Hello world from " << name << std::endl; //Print Hello world from "user name"
+    std::string name; //User name.
+    std::cin >> name; //Input user name.
+    std::cout << "Hello world from " << name << std::endl; //Print "Hello world from (user name)".
     }
