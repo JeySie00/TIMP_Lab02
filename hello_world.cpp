4918d437d70c70a1732ec320f07a445e8864be8c
JeySie00 <JeySie00>
2023-03-02 09:24:27 +0300

    Fixed

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 #include <string>
 
-using namespace std;
-
 int main(int argc, char** argv){
-    std:: string name;
-    std::cin >> name;
-    std:: string name; //User name
+    std::string name; //User name
     std::cin >> name; //Input user name
     std::cout << "Hello world from " << name << std::endl;
     }
