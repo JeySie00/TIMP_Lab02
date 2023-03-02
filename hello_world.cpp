7011cb3ac49338d1129cd440d99587c1f90716e5
JeySie00 <JeySie00>
2023-03-02 09:49:22 +0300

    Change code style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char** argv){
-    std::string name; //User name.
-    std::cin >> name; //Input user name.
-    std::cout << "Hello world from " << name << std::endl; //Print "Hello world from (user name)".
-    }
+int
+main(int argc, char** argv)
+{
+  std::string name; // User name
+  std::cin >> name; // Input user name
+  std::cout << "Hello world from " << name
+            << std::endl; // Print Hello world from "user name"
+}
